14dad0116ba3adccc602c6e2ef9b06b884725d50
Gwennaël Arbona <gwennael.arbona@gmail.com>
2022-06-18 16:54:11 +0200

    Overhaul table class to support multiple data columns

--- Source/Neutron/UI/Widgets/NeutronTable.h
+++ Source/Neutron/UI/Widgets/NeutronTable.h
@@ -4,7 +4,89 @@
 
 #include "Neutron/UI/NeutronUI.h"
 
-/** Abstract table class with value comparison */
+/*----------------------------------------------------
+    Table entry type
+----------------------------------------------------*/
+
+/** Table entry type */
+template <typename T>
+class TNeutronTableValue
+{
+public:
+
+	TNeutronTableValue(T Value, const FText& Unit = FText()) : CurrentValue(Value), ValueUnit(Unit)
+	{
+		ReferenceValue = GetDefaultValue();
+	}
+
+	TNeutronTableValue(T Value, T Reference, const FText& Unit) : CurrentValue(Value), ReferenceValue(Reference), ValueUnit(Unit)
+	{}
+
+	// Generic number-focused display
+	TPair<FText, FLinearColor> GetDisplay() const
+	{
+		// Fetch the style data
+		const FNeutronMainTheme& Theme = FNeutronStyleSet::GetMainTheme();
+		FNumberFormattingOptions Options;
+		Options.MaximumFractionalDigits            = 1;
+		FNumberFormattingOptions DifferenceOptions = Options;
+		DifferenceOptions.AlwaysSign               = true;
+
+		// Build the comparison data
+		FLinearColor Color = FLinearColor::White;
+		FText        Text  = FText::FromString(FText::AsNumber(CurrentValue, &Options).ToString() + " " + ValueUnit.ToString());
+		if (ReferenceValue >= 0 && CurrentValue != ReferenceValue)
+		{
+			Color = CurrentValue > ReferenceValue ? Theme.PositiveColor : Theme.NegativeColor;
+			Text  = FText::FromString(
+				 Text.ToString() + " (" + FText::AsNumber(CurrentValue - ReferenceValue, &DifferenceOptions).ToString() + ")");
+		}
+
+		return TPair<FText, FLinearColor>(Text, Color);
+	}
+
+	// Generic number-focused default
+	T GetDefaultValue() const
+	{
+		return -1;
+	}
+
+protected:
+
+	T     CurrentValue;
+	T     ReferenceValue;
+	FText ValueUnit;
+};
+
+// Text override
+template <>
+TPair<FText, FLinearColor> TNeutronTableValue<FText>::GetDisplay() const
+{
+	// Fetch the style data
+	const FNeutronMainTheme& Theme = FNeutronStyleSet::GetMainTheme();
+
+	// Build the comparison data
+	FLinearColor Color = FLinearColor::White;
+	if (!ReferenceValue.IsEmpty() && !CurrentValue.EqualTo(ReferenceValue))
+	{
+		Color = Theme.PositiveColor;
+	}
+
+	return TPair<FText, FLinearColor>(CurrentValue, Color);
+}
+
+// Text override
+FText TNeutronTableValue<FText>::GetDefaultValue() const
+{
+	return FText();
+}
+
+/*----------------------------------------------------
+    Actual table type
+----------------------------------------------------*/
+
+/** Generic table type */
+template <int32 ColumnCount>
 class SNeutronTable : public SCompoundWidget
 {
 	SLATE_BEGIN_ARGS(SNeutronTable) : _Width(FOptionalSize())
@@ -16,6 +98,9 @@ class SNeutronTable : public SCompoundWidget
 	SLATE_END_ARGS()
 
 public:
+
+	SNeutronTable() : SCompoundWidget(), EvenRow(true)
+	{}
 
 	void Construct(const FArguments& InArgs)
 	{
@@ -46,115 +131,87 @@ public:
 	/** Add a table header */
 	void AddHeader(FText Label, FText Details = FText())
 	{
+		AddHeaders(Label, {Details});
+	}
+
+	/** Add a table header */
+	void AddHeaders(FText Label, const TArray<FText>& Details)
+	{
 		const FNeutronMainTheme& Theme = FNeutronStyleSet::GetMainTheme();
 
-		// clang-format off
-		TableBox->AddSlot()
-		.Padding(Theme.VerticalContentPadding)
-		[
-			SNew(SBorder)
-			.BorderImage(&Theme.TableHeaderBackground)
-			[
-				SNew(SHorizontalBox)
+		TArray<TPair<FText, FLinearColor>> ColorValues;
+		for (const FText& Text : Details)
+		{
+			ColorValues.Add(TPair<FText, FLinearColor>(Text, FLinearColor::White));
+		}
 
-				+ SHorizontalBox::Slot()
-				[
-					SNew(SRichTextBlock)
-					.Text(Label)
-					.TextStyle(&Theme.InfoFont)
-					.DecoratorStyleSet(&FNeutronStyleSet::GetStyle())
-					+ SRichTextBlock::ImageDecorator()
-				]
-
-				+ SHorizontalBox::Slot()
-				[
-					SNew(SRichTextBlock)
-					.Text(Details)
-					.TextStyle(&Theme.InfoFont)
-					.DecoratorStyleSet(&FNeutronStyleSet::GetStyle())
-					+ SRichTextBlock::ImageDecorator()
-				]
-			]
-		];
-		// clang-format on
-
-		EvenRow = false;
+		AddRow(Label, ColorValues, Theme.InfoFont, true);
 	}
 
 	/** Add a numerical table entry */
-	void AddEntry(FText Label, float CurrentValue, float PreviousValue = -1, FText Unit = FText())
+	template <typename T>
+	void AddEntry(FText Label, TNeutronTableValue<T> Value)
 	{
-		// Fetch the style data
-		const FNeutronMainTheme& Theme = FNeutronStyleSet::GetMainTheme();
-		FNumberFormattingOptions Options;
-		Options.MaximumFractionalDigits            = 1;
-		FNumberFormattingOptions DifferenceOptions = Options;
-		DifferenceOptions.AlwaysSign               = true;
-
-		// Build the comparison data
-		FLinearColor Color = FLinearColor::White;
-		FText        Text  = FText::FromString(FText::AsNumber(CurrentValue, &Options).ToString() + " " + Unit.ToString());
-		if (PreviousValue >= 0 && CurrentValue != PreviousValue)
-		{
-			Color = CurrentValue > PreviousValue ? Theme.PositiveColor : Theme.NegativeColor;
-			Text  = FText::FromString(
-				 Text.ToString() + " (" + FText::AsNumber(CurrentValue - PreviousValue, &DifferenceOptions).ToString() + ")");
-		}
-
-		AddRow(Label, Text, Color);
+		AddEntries<T>(Label, {Value});
 	}
 
-	/** Add a textual table entry */
-	void AddEntry(FText Label, FText CurrentText, FText PreviousText = FText(), FText Unit = FText())
+	/** Add a numerical table entry */
+	template <typename T>
+	void AddEntries(FText Label, const TArray<TNeutronTableValue<T>>& Values)
 	{
-		// Fetch the style data
 		const FNeutronMainTheme& Theme = FNeutronStyleSet::GetMainTheme();
 
-		// Build the comparison data
-		FLinearColor Color = FLinearColor::White;
-		if (!PreviousText.IsEmpty() && !CurrentText.EqualTo(PreviousText))
+		TArray<TPair<FText, FLinearColor>> ColorValues;
+		for (const auto& Value : Values)
 		{
-			Color = Theme.PositiveColor;
+			ColorValues.Add(Value.GetDisplay());
 		}
 
-		AddRow(Label, CurrentText, Color);
+		AddRow(Label, ColorValues, Theme.MainFont, false);
 	}
 
 protected:
 
 	/** Add a generic row to the table */
-	void AddRow(FText Label, FText Value, FLinearColor Color = FLinearColor::White)
+	void AddRow(FText Label, const TArray<TPair<FText, FLinearColor>>& Values, const FTextBlockStyle& TextStyle, bool Header)
 	{
-		const FNeutronMainTheme& Theme = FNeutronStyleSet::GetMainTheme();
+		const FNeutronMainTheme&   Theme = FNeutronStyleSet::GetMainTheme();
+		TSharedPtr<SHorizontalBox> Row;
 
 		// clang-format off
 		TableBox->AddSlot()
 		.AutoHeight()
+		.Padding(Header ? Theme.VerticalContentPadding : FMargin(0))
 		[
 			SNew(SBorder)
-			.BorderImage(EvenRow ? &Theme.TableEvenBackground : &Theme.TableOddBackground)
+			.BorderImage(Header ? &Theme.TableHeaderBackground : (EvenRow ? &Theme.TableEvenBackground : &Theme.TableOddBackground))
 			[
-				SNew(SHorizontalBox)
+				SAssignNew(Row, SHorizontalBox)
 
 				+ SHorizontalBox::Slot()
 				[
 					SNew(SRichTextBlock)
 					.Text(Label)
-					.TextStyle(&Theme.MainFont)
-					.WrapTextAt(Width.Get() / 2)
+					.TextStyle(&TextStyle)
+					.WrapTextAt(Width.Get() / ColumnCount)
 					.DecoratorStyleSet(&FNeutronStyleSet::GetStyle())
 					+ SRichTextBlock::ImageDecorator()
 				]
-
-				+ SHorizontalBox::Slot()
-				[
-					SNew(STextBlock)
-					.Text(Value)
-					.TextStyle(&Theme.MainFont)
-					.ColorAndOpacity(Color)
-				]
 			]
 		];
+		
+		for (const TPair<FText, FLinearColor>& TextAndColor : Values)
+		{
+			Row->AddSlot()
+			[
+				SNew(STextBlock)
+				.Text(TextAndColor.Key)
+				.TextStyle(&TextStyle)
+				.WrapTextAt(Width.Get() / ColumnCount)
+				.ColorAndOpacity(TextAndColor.Value)
+			];
+		}
+
 		// clang-format on
 
 		EvenRow = !EvenRow;
