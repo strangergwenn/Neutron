ede5581e124f0d718316080373ead75dee5a5b2e
Gwennaël Arbona <gwennael.arbona@gmail.com>
2022-11-12 09:05:34 +0100

    Add clang-format

---
+++ Format.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#-------------------------------------------------------------------------------
+# Format sources using clang-format
+# Usage : Format.py
+# 
+# Gwennaël Arbona 2022
+#-------------------------------------------------------------------------------
+
+import glob
+import json
+import os
+import re
+import subprocess
+
+#-------------------------------------------------------------------------------
+# Define run settings
+#-------------------------------------------------------------------------------
+
+sourceDir = 'Source'
+scriptDir = os.getcwd()
+
+#-------------------------------------------------------------------------------
+# Run formatting
+#-------------------------------------------------------------------------------
+	
+for root, directories, filenames in os.walk(sourceDir):
+	for filename in filenames:		
+		if re.match('.*\.((h)|(c)|(cpp))$', filename):
+			subprocess.check_call([				
+				'clang-format',
+				'-i',
+				'-style=file',
+				str(os.path.join(root, filename))
+			])
+	
+os.chdir(scriptDir)
